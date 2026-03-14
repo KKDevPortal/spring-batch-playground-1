054a61dd4eaa3890ffd5f0635659aa2482ceaa13
Kaushal Kishor <kaushal.kishor@niit.com>
2026-03-14 20:47:55 +0530

    csvfileprocessing

--- src/main/java/com/kkdevportal/spring_batch_playground_1/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kkdevportal.spring_batch_playground_1.repository;
-
-import com.kkdevportal.spring_batch_playground_1.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
