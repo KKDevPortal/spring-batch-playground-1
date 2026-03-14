afff0b6285ad2b3692fcfeca8b2016ef0db94d85
Kaushal Kishor <kaushal.kishor@niit.com>
2026-03-14 11:19:36 +0530

    project init

--- src/main/java/com/kkdevportal/spring_batch_playground_1/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kkdevportal.spring_batch_playground_1.repository;
-
-import com.kkdevportal.spring_batch_playground_1.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
